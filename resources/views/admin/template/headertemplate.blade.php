ce4f628d17a47b63224a9ad667f3de378bb4f952
PilotJinix <agungsantoso557@gmail.com>
2021-07-08 07:36:06 +0700

    jquerry change

--- resources/views/admin/template/headertemplate.blade.php
+++ resources/views/admin/template/headertemplate.blade.php
@@ -27,11 +27,12 @@
 
 
 <!-- JAVASCRIPT -->
+    <script src="{{asset("assets/admin/libs/jquery/jquery.min.js")}}"></script>
 <script src="{{asset("assets/admin/libs/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
 <script src="{{asset("assets/admin/libs/metismenu/metisMenu.min.js")}}"></script>
 <script src="{{asset("assets/admin/libs/simplebar/simplebar.min.js")}}"></script>
 <script src="{{asset("assets/admin/libs/node-waves/waves.min.js")}}"></script>
-<script src="{{asset("assets/admin/libs/jquery/jquery.min.js")}}"></script>
+
 
 <!-- apexcharts -->
 <script src="{{asset("assets/admin/libs/apexcharts/apexcharts.min.js")}}"></script>
